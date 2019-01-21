6f48e1384728a4f51bfc3ae660ad5e675025e87f
kurtmilla1994 <kurtmilla1994@gmail.com>
2019-01-21 14:40:23 -0500

    update 1.0

---
+++ app.js
@@ -0,0 +1,74 @@
+/*****************************************
+ ***** Javascript CheatSheet LIBRARY *****
+ *****************************************/
+
+// // log to console
+// //single line comment
+// /*
+// multi
+// line
+// comment
+// */
+// //console
+// console.log('Hello world');
+// console.log(1994);
+// console.log(true);
+// var greeting = 'hello';
+// console.log(greeting);
+// console.log(1,2,3,4);
+// console.log({a:1, b:2});
+// console.table({a:1, b:2});
+// console.error("Error expected");//error appears Red
+// console.clear();
+// console.warn('this is a warning');//warning appears yellow
+// console.time('Hello');
+// console.log('Hello world');
+// console.log('Hello world');
+// console.log('Hello world');
+// console.log('Hello world');
+// console.log('Hello world');
+// console.log('Hello world');
+// console.timeEnd('Hello');
+// //variables (var, let, const)
+// var name = 'Kurt Miller';  //using var or let you can reassign variables
+// console.log(name);         //outputs Kurt Miller
+// name = 'steve smith';       
+// console.log(name);         //outputs steve smith
+
+// //init var
+// var greeting
+// greeting = 'hello';
+// console.log(greeting);
+
+// //variables can include letters, numbers, _, and $
+// //cannot start with a number!
+// //camelCase (multi word variables)
+// var firstName = 'john'; //cameCase
+// var first_name = 'john'; //underscore
+// var Firstname = 'john'; //Pascalcase good practice to use for creating Classes and Constructors
+
+//highlight all code and CTRL+ / comments everything out
+// let name; //easier to make temporary changes
+// name = 'Kurt Miller';  //using var or let you can reassign variables
+// console.log(name);         //outputs Kurt Miller
+// name = 'steve smith';       
+// console.log(name);
+
+//const is permanent
+// const name = 'Kurt';
+// console.log(name);
+//cannot reassign
+// have to assign a value
+
+// const person = { // object
+//     name: 'john',
+//     age: 30,
+//     desc: 'Young Male'
+// }
+// person.name = 'Sara';
+// person.age = 32;
+// console.log(person);
+
+// const numbers = [1,2,3,4,5]; //set number array
+// numbers.push(6,7,8,9); //pushes numbers and adds to array
+// console.log(numbers);
